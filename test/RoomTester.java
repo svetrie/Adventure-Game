1b68a9ab155311d7837b42103fa60c2ea159d5db
svetrie <vetsenthilkumar@gmail.com>
2018-02-13 14:27:44 -0600

    Added more test cases for Room class

--- test/RoomTester.java
+++ test/RoomTester.java
@@ -5,40 +5,70 @@ import com.example.JsonFileLoader;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 public class RoomTester {
     private static GameWorld gameWorld;
-    private static Room siebel1112;
+    private static Room noyesLab;
 
     @Before
     public void setUp() {
-        gameWorld = JsonFileLoader.parseJsonFileUsingUrl(DEFAULT_JSON_FILE_URL);
-        siebel1112 = gameWorld.getRooms()[4];
+        gameWorld = JsonFileLoader.parseJsonFileUsingFilePath("AlternateAdventure.json");
+        noyesLab = gameWorld.getRooms()[4];
     }
 
     @Test
     public void getNameTest() {
-        assertEquals("Siebel1112", siebel1112.getName());
+        assertEquals("NoyesLaboratory", noyesLab.getName());
     }
 
     @Test
     public void getDescriptionTest() {
-        assertEquals("You are in Siebel 1112.  There is space for two code reviews in this room.",
-                siebel1112.getDescription());
+        assertEquals("You are in Noyes Laboratory. A mob of students exit Noyes 100 as their lecture ends",
+                noyesLab.getDescription());
     }
 
     @Test
     public void getItemsTest() {
-        assertTrue(Arrays.asList("USB-C connector", "grading rubric").equals(siebel1112.getItems()));
+        String[] expectedItemNames = {"lab coat", "beaker", "boxing gloves"};
+
+        for (int i = 0; i < noyesLab.getItems().size(); i++) {
+            assertEquals(expectedItemNames[i], noyesLab.getItems().get(i).getName());
+        }
     }
 
     @Test
-    public void getDirectionTest() {
-        assertTrue(siebel1112.getDirections()[0].getDirectionName().equals("West"));
-        assertTrue(siebel1112.getDirections()[0].getRoom().equals("SiebelNorthHallway"));
+    public void getItemByNameTest() {
+        assertEquals(250, noyesLab.getItemByName("lab coat").getDamage(), 0.0001);
+    }
+
+    @Test
+    public void getMonstersInRoomTest() {
+        assertEquals(Arrays.asList("Connor McGregor", "Floyd Mayweather"), noyesLab.getMonstersInRoom());
+    }
+
+    @Test
+    public void removeMonsterTest() {
+        noyesLab.removeMonster("Connor McGregor");
+        noyesLab.removeMonster("Floyd Mayweather");
+
+        assertTrue(noyesLab.getMonstersInRoom().size() == 0);
+    }
+
+    @Test
+    public void areAllMonstersDefeatedTest() {
+        assertTrue(!noyesLab.areAllMonstersDefeated());
+
+        noyesLab.removeMonster("Connor McGregor");
+        noyesLab.removeMonster("Floyd Mayweather");
+
+        assertTrue(noyesLab.areAllMonstersDefeated());
+    }
+
+    @Test
+    public void getValidDirectionTest() {
+        assertEquals("IlliniUnion", noyesLab.getValidDirection("Northwest").getRoom());
     }
 }
